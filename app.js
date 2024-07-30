e9a030604b8c8a0c723e7142abd104d1de6b46d7
mrodriguezul <miguel.rodriguez.u@gmail.com>
2024-07-30 05:55:51 -0500

    Se actualiza valor de retorno de funciones anteriores

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var app = function(){
     var dato = "sdsd";
     function calculo1(){
-        return true;
-    };
-    function calculo2(){
         return false;
     };
-    function calculo3(){
+    function calculo2(){
         return false;
     };
 };
